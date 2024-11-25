bc93b5e374c88d46ca4df9e831f2611c3abec501
Your Name <@shareenbangi@gmail.com>
2024-11-26 00:38:07 +0700

    Update index.html and index.js

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Menggunakan express untuk menyajikan file statis (seperti index.html)
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'BANSOSIDN24-25')));
 
 // Menyajikan index.html untuk permintaan ke root ("/")
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'BANSOSIDN24-25', 'index.html'));
 });
 
 // Mulai server
