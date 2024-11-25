59403fa3522d6a2def1847e5c75ffe6b5602bdb7
Your Name <@shareenbangi@gmail.com>
2024-11-26 00:31:05 +0700

    Set up Express server to serve index.html

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-// index.js
-console.log("Aplikasi berjalan!");
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Tentukan port untuk aplikasi
+const port = process.env.PORT || 3000;
+
+// Menggunakan express untuk menyajikan file statis (seperti index.html)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Menyajikan index.html untuk permintaan ke root ("/")
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Mulai server
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
