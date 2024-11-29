a7af55e7ee529fec7a5e97aab19af6c35152caac
Your Name <ashiksino84@gmail.com>
2024-11-29 23:06:00 +0700

    Update index.js and package.json, add index.html

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files from the 'public' folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Serve the index.html file when visiting the root URL
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.send('Hello, World!');
 });
 
-// Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
